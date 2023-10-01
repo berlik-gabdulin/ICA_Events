b414d716db12d11170cd4d44ddb929cec7b127ae
Berlik Gabdulin <icepanda@MacBook-Air-Berlik.local>
2023-10-02 02:01:33 +0600

    Light Version

---
+++ next.config.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const fs = require('graceful-fs');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+fs.gracefulify(require('fs'));
+
+const nextConfig = {
+  env: {
+    API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
+  images: {
+    domains: ['onsite.iteca.kz', 'exhibitions.az', 'iteca.uz'], // разрешает загрузку изображений с основного домена
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
