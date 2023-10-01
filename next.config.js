b303eccd32ca450a524aaafb1fccc4f1a4d33cdd
Berlik Gabdulin <icepanda@MacBook-Air-Berlik.local>
2023-10-02 02:46:10 +0600

    Light Version

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const fs = require('graceful-fs');
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-fs.gracefulify(require('fs'));
-
 const nextConfig = {
   env: {
     API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
